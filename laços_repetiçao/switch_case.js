7eee456aca20660314edfd682ce248a8e3b5fd3f
AdlerCarlos30 <adler.carlos.18@gmail.com>
2025-06-13 11:44:35 -0400

    switch_case

---
+++ laços_repetiçao/switch_case.js
@@ -0,0 +1,26 @@
+var dia = "quarta";
+
+switch (dia) {
+    case "segunda":
+    console.log("Dia de Começar a Semana");
+    break;
+
+    case "terça":
+    console.log("Hora de Aprender Coisas Novas");
+    break;
+
+    case "quarta":
+    console.log("Metade da semana, continue firme");
+    break;
+
+    case "quinta":
+    console.log("Quase lá, mais um pouco de esorço");
+    break;
+
+    case "sexta":
+    console.log("Dia de comemorar, o fim de semana chegou");
+    break;
+
+    default:
+    console.log("Final de Semana Chegou")
+}
