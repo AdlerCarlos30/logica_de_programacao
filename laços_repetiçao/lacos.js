516aea58fc847e84cecf90062635f10fe7c414ed
AdlerCarlos30 <adler.carlos.18@gmail.com>
2025-06-13 10:32:55 -0400

    feat_do_while

--- laços_repetiçao/lacos.js
+++ laços_repetiçao/lacos.js
@@ -4,4 +4,4 @@ do{
     numero = prompt ('Digite um número entre 1 e 20:');
 } while(numero < 1 || numero > 20);
 
-console.log (`Voce digitou o número ${numero} .`);
+console.log (`Voce digitou o número ${numero} .`); 
